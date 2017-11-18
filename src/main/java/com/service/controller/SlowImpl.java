cca21e85e28230df3112231ae8139f2815b0d17a
paas <paas@cse-codegen-backend-886071295-2qtv3>
2017-11-18 07:20:16 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/controller/SlowImpl.java
@@ -0,0 +1,65 @@
+package com.service.controller;
+
+import com.service.model.Person;
+
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import io.servicecomb.provider.rest.common.RestSchema;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CseSpringCodegen", date = "2017-11-18T07:20:16.200Z")
+
+@RestSchema(schemaId = "slow")
+@RequestMapping(path = "/dumpname", produces = MediaType.APPLICATION_JSON)
+public class SlowImpl {
+
+    @Autowired
+    private SlowDelegate userSlowDelegate;
+
+
+    @RequestMapping(value = "/add",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public Integer add( @RequestParam(value = "a", required = true) Integer a, @RequestParam(value = "b", required = true) Integer b){
+
+        return userSlowDelegate.add(a, b);
+    }
+
+
+    @RequestMapping(value = "/sayhei",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String sayHei( @RequestHeader(value="name", required=true) String name){
+
+        return userSlowDelegate.sayHei(name);
+    }
+
+
+    @RequestMapping(value = "/sayhello/{name}",
+        produces = { "application/json" }, 
+        method = RequestMethod.POST)
+    public String sayHello( @PathVariable("name") String name){
+
+        return userSlowDelegate.sayHello(name);
+    }
+
+
+    @RequestMapping(value = "/sayhi",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String sayHi( @RequestParam(value = "name", required = true) String name){
+
+        return userSlowDelegate.sayHi(name);
+    }
+
+
+    @RequestMapping(value = "/saysomething",
+        produces = { "application/json" }, 
+        method = RequestMethod.POST)
+    public String saySomething( @RequestParam(value = "prefix", required = true) String prefix, @RequestBody Person user){
+
+        return userSlowDelegate.saySomething(prefix, user);
+    }
+
+}
---
+++ src/test/java/com/service/controller/TestSlow.java
@@ -0,0 +1,87 @@
+package com.service.controller;
+
+import com.service.model.Person;
+
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import io.servicecomb.foundation.common.utils.BeanUtils;
+import io.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class TestSlow {
+
+    SlowImpl slowImpl;
+
+    @Before
+    public void setup() throws Exception{
+        Log4jUtils.init();
+        BeanUtils.init();
+        slowImpl = BeanUtils.getBean("slowImpl");
+    }
+
+
+
+    @Test
+    public void testadd(){
+
+        Integer expactReturnValue = null; // You should put the expect Integer type value here.
+
+        // You should put the parameter here, in such footprint: slowImpl.add(Integer a, Integer b)
+        Integer returnValue = slowImpl.add(null, null);
+
+        assertEquals(expactReturnValue, returnValue);
+    }
+
+
+    @Test
+    public void testsayHei(){
+
+        String expactReturnValue = null; // You should put the expect String type value here.
+
+        // You should put the parameter here, in such footprint: slowImpl.sayHei(String name)
+        String returnValue = slowImpl.sayHei(null);
+
+        assertEquals(expactReturnValue, returnValue);
+    }
+
+
+    @Test
+    public void testsayHello(){
+
+        String expactReturnValue = null; // You should put the expect String type value here.
+
+        // You should put the parameter here, in such footprint: slowImpl.sayHello(String name)
+        String returnValue = slowImpl.sayHello(null);
+
+        assertEquals(expactReturnValue, returnValue);
+    }
+
+
+    @Test
+    public void testsayHi(){
+
+        String expactReturnValue = null; // You should put the expect String type value here.
+
+        // You should put the parameter here, in such footprint: slowImpl.sayHi(String name)
+        String returnValue = slowImpl.sayHi(null);
+
+        assertEquals(expactReturnValue, returnValue);
+    }
+
+
+    @Test
+    public void testsaySomething(){
+
+        String expactReturnValue = null; // You should put the expect String type value here.
+
+        // You should put the parameter here, in such footprint: slowImpl.saySomething(String prefix, Person user)
+        String returnValue = slowImpl.saySomething(null, null);
+
+        assertEquals(expactReturnValue, returnValue);
+    }
+
+}
+
+
+
